8e106f9543b94754ff6df32e14734fb40717b6d2
Alex Reinoso <ralexjessiel@gmail.com>
2020-05-25 13:44:01 -0500

    Creado Servlets
    falta agregar funcionalidades

---
+++ Programa/src/ec/edu/ups/controlador/LogOut.java
@@ -0,0 +1,58 @@
+package ec.edu.ups.controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import ec.edu.ups.dao.DAOFactory;
+import ec.edu.ups.dao.TelefonoDAO;
+import ec.edu.ups.dao.UsuarioDAO;
+import ec.edu.ups.entidad.Usuario;
+
+/**
+ * Servlet implementation class LogOut
+ */
+@WebServlet("/LogOut")
+public class LogOut extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogOut() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		TelefonoDAO telefonoDAO = DAOFactory.getFactory().getTelefonoDAO();
+		HttpSession sesion = request.getSession();
+		Usuario usuario = new Usuario();
+		UsuarioDAO usuarioDAO = DAOFactory.getFactory().getUsuarioDAO();
+		
+		sesion.setAttribute("accesos", sesion.getAttribute("accesos"));
+		
+		if(Integer.parseInt("id")==1) {
+			if(request.getParameter("correo") != null) {
+				System.out.println("Correo: "+request.getParameter("correo"));
+			}
+		}
+	}
+
+}
